bb9f2473d7d69f8391c53ff5504b43130c3a21dd
MaG2k20 <62188958+MaG2k20@users.noreply.github.com>
2023-09-27 01:00:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="style.css"/>
+      <title> FlappyLicky</title>
+    </head>
+
+    <body>
+        <div class="header">
+          <h1 class="titolo"> FlappyLicky.it </h1>
+          <h4 class="subTitolo"> il miglior gioco in italia </h4>
+        </div>
+
+        <div class="center">
+          <?php
+           include("index-unity.php");
+          ?>
+          
+        </div>
+
+        <div class="footer">
+        </div>
+    </body>
+</html>
